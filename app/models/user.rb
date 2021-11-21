c1672d5e22620e906feff11d45ea710ae2acc818
undr0id <kamendimitrov97@gmail.com>
2021-11-21 17:48:34 +0200

    Added sessions and users

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+    before_save {self.email = email.downcase}
+    VALID_EMAIL_REGEX = /\A^(.+)@(.+)$\z/
+    VALID_USERNAME_REGEX = /\A[0-9]*|[A-Za-z]*\z/
+
+    # Username Validation is here:
+    validates :username, 
+        presence: true, 
+        uniqueness: { case_sensitive: false }, 
+        length: {minimum: 3, maximum: 50}, 
+        format: { with: VALID_USERNAME_REGEX, message: 'Please use only the following characters: ":a-z, A-Z, 0-9, ., _ ".'}
+    # inclusion: { in: VALID_USERNAME_REGEX}
+    
+    # email Validation is here:
+    validates :email, 
+        presence: true, 
+        uniqueness: { case_sensitive: false }, 
+        length: {minimum: 3, maximum: 50}, 
+        format: { with: VALID_EMAIL_REGEX, message: 'Please enter a valid email address.'}
+    
+    # password Validation is here:
+    has_secure_password
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
   root "page#home"
   get '/about', to: "page#about"
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  get '/logout', to: "sessions#destroy"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
